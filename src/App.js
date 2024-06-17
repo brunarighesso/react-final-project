4a7e859a496f41ebccbaa3fb1cdeb2ebd014e84b
brunarighesso <157367684+brunarighesso@users.noreply.github.com>
2024-06-17 11:02:16 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/bruna-righesso-23175622/"
-          target="_blank"
-        >
-          Bruna Righesso
-        </a>{" "}
-        and is open sourced{" "}
-        <a
-          href="https://github.com/brunarighesso/react-final-project"
-          target="_blank"
-        >
-          on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/bruna-righesso-23175622/"
+            target="_blank"
+          >
+            Bruna Righesso
+          </a>{" "}
+          and is open sourced{" "}
+          <a
+            href="https://github.com/brunarighesso/react-final-project"
+            target="_blank"
+          >
+            on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
