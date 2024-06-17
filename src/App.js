cdca752d5bc56d0d79a0aa3ccf4d6bd322d272dc
brunarighesso <157367684+brunarighesso@users.noreply.github.com>
2024-06-17 10:49:32 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/bruna-righesso-23175622/"
+          target="_blank"
+        >
+          Bruna Righesso
+        </a>{" "}
+        and is open sourced{" "}
+        <a
+          href="https://github.com/brunarighesso/react-final-project"
+          target="_blank"
+        >
+          on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
