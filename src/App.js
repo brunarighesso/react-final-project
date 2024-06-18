a3302f99b67c87b6354569c93affe74b713cd7d4
brunarighesso <157367684+brunarighesso@users.noreply.github.com>
2024-06-18 09:40:45 +0200

    added weather data

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         <h1>Weather App</h1>
         <Weather />
         <footer>
-          This project was coded by{" "}
+          This project was coded by {" "}
           <a
             href="https://www.linkedin.com/in/bruna-righesso-23175622/"
             target="_blank"
